d565a08a19cd061ae0a667b8fdfca22483e92b26
Sanketh Kamath <sankethkamath99@gmail.com>
2016-11-26 20:56:29 -0700

    working redirect

--- Clipper_V_0/urls.py
+++ Clipper_V_0/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('core.urls', namespace='core'))
+    url(r'^', include('core.urls', namespace='core')),
+    url(r'^stylist/', include('stylist.urls', namespace='stylist'))
 ]
--- stylist/views.py
+++ stylist/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def profile(request):
 	full_name = request.user.get_full_name()
-	render(request, 'stylist/profile.html', {'full_name': full_name})
+	return render(request, 'stylist/profile.html', {'full_name': full_name})
