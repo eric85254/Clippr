5ceaed18c65f1bf3f9e0a9ab154300b78f80692f
Sanketh Kamath <sankethkamath99@gmail.com>
2017-01-25 16:15:30 -0700

    Modified admin.py and models.py of core package.

--- core/models.py
+++ core/models.py
@@ -19,9 +19,12 @@ class User(AbstractUser):
 class Menu(models.Model):
     name = models.CharField(max_length=20)
     category = models.TextField(default='MAIN')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     picture = models.FileField(upload_to='menu/%Y/%m/%d', null=True, blank=True)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return "Category: " + self.category + " || Option: " + self.name
 
 
 class Bill(models.Model):
