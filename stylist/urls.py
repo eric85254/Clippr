957f974d7b84d2e817db3f8471601f7fa6ada252
Sanketh Kamath <sankethkamath99@gmail.com>
2017-01-26 22:18:00 -0700

    Added ability for stylists to add pictures to their portfolio. Note pictures are not connected to a menu item.

--- stylist/urls.py
+++ stylist/urls.py
@@ -4,5 +4,6 @@ from stylist import views
 
 urlpatterns = [
     url(r'profile/$', views.profile, name="profile"),
-    url(r'dashboard/$', views.dashboard, name="dashboard")
+    url(r'dashboard/$', views.dashboard, name="dashboard"),
+    url(r'upload_haircut/$', views.upload_haircut, name="upload_haircut")
 ]
--- stylist/views.py
+++ stylist/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
 
 from core.models import Appointment
+from stylist.forms import NewPortfolioHaircutForm
+from stylist.models import PortfolioHaircut
 
 
 def profile(request):
     if request.user.is_stylist == 'YES':
         full_name = request.user.get_full_name()
-        if request.user.profile_picture is not None:
-            stylist = request.user
-        else:
-            stylist = None
+
+        portfolio_haircuts = PortfolioHaircut.objects.filter(stylist=request.user)
+
         return render(request, 'stylist/profile.html',
                       {'full_name': full_name,
-                       'stylist': stylist})
+                       'stylist': request.user,
+                       'portfolio_haircuts': portfolio_haircuts},)
     else:
         return redirect('core:logout')
 
@@ -25,5 +27,23 @@ def dashboard(request):
             appointment_list = None
         return render(request, 'stylist/dashboard.html', {'full_name': request.user.get_full_name(),
                                                           'appointments': appointment_list})
+    else:
+        return redirect('core:logout')
+
+def upload_haircut(request):
+    if request.user.is_stylist == 'YES':
+        if request.method == 'POST':
+            new_portfolioHaircut_form = NewPortfolioHaircutForm(request.POST)
+
+            if new_portfolioHaircut_form.is_valid():
+                new_portfolioHaircut = new_portfolioHaircut_form.save(commit=False)
+                new_portfolioHaircut.stylist = request.user
+                new_portfolioHaircut.picture = request.FILES['picture']
+                new_portfolioHaircut.save()
+
+            return redirect('stylist:profile')
+        else:
+            return render(request, 'stylist/upload_haircut.html')
+
     else:
         return redirect('core:logout')
