316c7dab51311e77f3ae5893adbc5e50b2abf19f
Sanketh Kamath <sankethkamath99@gmail.com>
2017-01-31 19:04:06 -0700

    Added ability for stylists to reschedule appointments.

--- stylist/urls.py
+++ stylist/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'dashboard/$', views.dashboard, name="dashboard"),
     url(r'upload_haircut/$', views.upload_haircut, name="upload_haircut"),
     url(r'accept_appointment/$', views.accept_appointment, name='accept_appointment'),
-    url(r'decline_appointment/$', views.decline_appointment, name='decline_appointment')
+    url(r'decline_appointment/$', views.decline_appointment, name='decline_appointment'),
+    url(r'reschedule_appointment/$', views.reschedule_appointment, name='reschedule_appointment')
 ]
--- stylist/views.py
+++ stylist/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render, redirect
 
 from core.models import Appointment, Menu
@@ -14,21 +16,26 @@ def profile(request):
         return render(request, 'stylist/profile.html',
                       {'full_name': full_name,
                        'stylist': request.user,
-                       'portfolio_haircuts': portfolio_haircuts},)
+                       'portfolio_haircuts': portfolio_haircuts}, )
     else:
         return redirect('core:logout')
+
 
 def dashboard(request):
     if request.user.is_stylist == 'YES':
         pending_appointments = Appointment.objects.filter(stylist=request.user, status=Appointment.STATUS_PENDING)
         accepted_appointments = Appointment.objects.filter(stylist=request.user, status=Appointment.STATUS_ACCEPTED)
         declined_appointments = Appointment.objects.filter(stylist=request.user, status=Appointment.STATUS_DECLINED)
+        rescheduled_bystylist_appointments = Appointment.objects.filter(stylist=request.user,
+                                                                        status=Appointment.STATUS_RECHEDULED_BYSTYLIST)
         return render(request, 'stylist/dashboard.html', {'full_name': request.user.get_full_name(),
                                                           'pending_appointments': pending_appointments,
                                                           'accepted_appointments': accepted_appointments,
-                                                          'declined_appointments': declined_appointments})
+                                                          'declined_appointments': declined_appointments,
+                                                          'rescheduled_bystylist_appointments': rescheduled_bystylist_appointments})
     else:
         return redirect('core:logout')
+
 
 def upload_haircut(request):
     if request.user.is_stylist == 'YES':
@@ -61,7 +68,8 @@ def accept_appointment(request):
             appointment = Appointment.objects.get(pk=request.POST.get('appointment_pk'))
             appointment.status = Appointment.STATUS_ACCEPTED
             appointment.save()
-    return redirect(request.META.get('HTTP_REFERER'))
+        return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('core:logout')
 
 
 def decline_appointment(request):
@@ -70,4 +78,16 @@ def decline_appointment(request):
             appointment = Appointment.objects.get(pk=request.POST.get('appointment_pk'))
             appointment.status = Appointment.STATUS_DECLINED
             appointment.save()
-    return redirect(request.META.get('HTTP_REFERER'))
+        return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('core:logout')
+
+
+def reschedule_appointment(request):
+    if request.user.is_stylist == 'YES':
+        if request.method == 'POST':
+            appointment = Appointment.objects.get(pk=request.POST.get('appointment_pk'))
+            appointment.status = Appointment.STATUS_RECHEDULED_BYSTYLIST
+            appointment.date = datetime.strptime(request.POST.get('date'), '%Y-%m-%dT%H:%M')
+            appointment.save()
+        return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('core:logout')
