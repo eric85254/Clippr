806acae5ca3e8431c5c5bec3f14ddf4fdf92cfe7
Sanketh Kamath <sankethkamath99@gmail.com>
2017-02-04 00:06:11 -0700

    Fixed Huge URL Bug.

--- administration/urls.py
+++ administration/urls.py
@@ -4,13 +4,13 @@ from administration import views
 # ToDo: Interesting Error.. if the url name for views.profile is r'home/$' it redirects to the actual home page and
 # does not go to views.profile. Figure this out.
 urlpatterns = [
-    url(r'profile/$', views.profile, name="profile"),
-    url(r'view_stylist_applications/$', views.view_stylist_applications, name="view_stylist_applications"),
-    url(r'schedule_interview/$', views.schedule_interview, name="schedule_interview"),
-    url(r'view_interviews/$', views.view_interviews, name="view_interviews"),
-    url(r'view_rejects/$', views.view_rejects, name="view_rejects"),
-    url(r'reinstate_application/$', views.reinstate_application, name="reinstate_application"),
-    url(r'reject_applicant/$', views.reject_applicant, name="reject_applicant"),
-    url(r'approve_applicant/$', views.approve_applicant, name="approve_applicant"),
-    url(r'view_stylists/$', views.view_stylists, name="view_stylists")
+    url(r'^profile/$', views.profile, name="profile"),
+    url(r'^view_stylist_applications/$', views.view_stylist_applications, name="view_stylist_applications"),
+    url(r'^schedule_interview/$', views.schedule_interview, name="schedule_interview"),
+    url(r'^view_interviews/$', views.view_interviews, name="view_interviews"),
+    url(r'^view_rejects/$', views.view_rejects, name="view_rejects"),
+    url(r'^reinstate_application/$', views.reinstate_application, name="reinstate_application"),
+    url(r'^reject_applicant/$', views.reject_applicant, name="reject_applicant"),
+    url(r'^approve_applicant/$', views.approve_applicant, name="approve_applicant"),
+    url(r'^view_stylists/$', views.view_stylists, name="view_stylists")
 ]
--- stylist/urls.py
+++ stylist/urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import url
 from stylist import views
 
 urlpatterns = [
-    url(r'profile/$', views.profile, name="profile"),
-    url(r'dashboard/$', views.dashboard, name="dashboard"),
-    url(r'upload_haircut/$', views.upload_haircut, name="upload_haircut"),
-    url(r'accept_appointment/$', views.accept_appointment, name='accept_appointment'),
-    url(r'decline_appointment/$', views.decline_appointment, name='decline_appointment'),
-    url(r'reschedule_appointment/$', views.reschedule_appointment, name='reschedule_appointment'),
-    url(r'complete_appointment/$', views.complete_appointment, name='complete_appointment'),
-    url(r'view_bill/$', views.view_bill, name='view_bill'),
-    url(r'delete_item/$', views.delete_item, name='delete_item'),
-    url(r'add_item/$', views.add_item, name='add_item'),
-    url(r'edit_portfoliohaircut/$', views.edit_portfoliohaircut, name='edit_portfoliohaircut')
+    url(r'^profile/$', views.profile, name="profile"),
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^upload_haircut/$', views.upload_haircut, name="upload_haircut"),
+    url(r'^accept_appointment/$', views.accept_appointment, name='accept_appointment'),
+    url(r'^decline_appointment/$', views.decline_appointment, name='decline_appointment'),
+    url(r'^reschedule_appointment/$', views.reschedule_appointment, name='reschedule_appointment'),
+    url(r'^complete_appointment/$', views.complete_appointment, name='complete_appointment'),
+    url(r'^view_bill/$', views.view_bill, name='view_bill'),
+    url(r'^delete_item/$', views.delete_item, name='delete_item'),
+    url(r'^add_item/$', views.add_item, name='add_item'),
+    url(r'^edit_portfoliohaircut/$', views.edit_portfoliohaircut, name='edit_portfoliohaircut')
 ]
