82d109f670cf4aa4cfbf4e74f4bf831d9bda7bd4
Sanketh Kamath <sankethkamath99@gmail.com>
2017-02-06 17:23:15 -0700

    cleaned up Stylist Urls.

--- stylist/urls.py
+++ stylist/urls.py
@@ -1,19 +1,31 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from stylist import views
 
-urlpatterns = [
-    url(r'^profile/$', views.profile, name="profile"),
-    url(r'^dashboard/$', views.dashboard, name="dashboard"),
-    url(r'^upload_haircut/$', views.upload_haircut, name="upload_haircut"),
+appointmentpatterns = [
+    url(r'^$', views.dashboard, name="dashboard"),
     url(r'^accept_appointment/$', views.accept_appointment, name='accept_appointment'),
     url(r'^decline_appointment/$', views.decline_appointment, name='decline_appointment'),
     url(r'^reschedule_appointment/$', views.reschedule_appointment, name='reschedule_appointment'),
     url(r'^complete_appointment/$', views.complete_appointment, name='complete_appointment'),
+    url(r'^submit_review/$', views.submit_review, name='submit_review'),
+]
+
+billpatterns = [
     url(r'^view_bill/$', views.view_bill, name='view_bill'),
     url(r'^delete_item/$', views.delete_item, name='delete_item'),
     url(r'^add_item/$', views.add_item, name='add_item'),
-    url(r'^edit_portfoliohaircut/$', views.edit_portfoliohaircut, name='edit_portfoliohaircut'),
-    url(r'^submit_review/$', views.submit_review, name='submit_review'),
     url(r'^add_haircut/$', views.add_haircut, name='add_haircut')
+]
+
+profilepatterns = [
+    url('^$', views.profile, name="profile"),
+    url(r'^upload_haircut/$', views.upload_haircut, name="upload_haircut"),
+    url(r'^edit_portfoliohaircut/$', views.edit_portfoliohaircut, name='edit_portfoliohaircut'),
+]
+
+urlpatterns = [
+    url(r'^profile/', include(profilepatterns)),
+    url(r'^appointments/', include(appointmentpatterns)),
+    url(r'^appointment/bill/', include(billpatterns))
 ]
