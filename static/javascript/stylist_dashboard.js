f408a223f5b845ba3892cb82ecb7b25adad56ef2
Lynchboxed <adamlynch95@gmailcom>
2017-02-26 21:17:56 -0700

    Made menu options collapseable          -Currently broken  Made portfolio modal hide on escape key press          -There is a bug, if you press escape without the image expanded, it no longer pops up

--- static/javascript/stylist_dashboard.js
+++ static/javascript/stylist_dashboard.js
@@ -92,3 +92,7 @@ function initMap() {
 function showMarkerOnMap(button) {
     geocodeAddress(geocoder, map, button.value); //address is located in button value
 }
+
+$(".menu-option").click(function() {
+    $("#collapseTwo").collapse('toggle');
+})
