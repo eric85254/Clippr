70e4d2bbcbaefff8232e62dd5c21f6d599343536
Sanketh Kamath <sankethkamath99@gmail.com>
2017-03-02 02:56:56 -0700

    createDefaultHaircuts management command added.

--- core/utils/global_constants.py
+++ core/utils/global_constants.py
@@ -1,2 +1,6 @@
 DEFAULT_PICTURE_LOCATION = "\defaults\default_user.png"
 DEFAULT_MENU_PICTURE = "\defaults\default_menu.png"
+
+DEFAULT_HAIRCUT_1 = "\defaults\haircut.jpeg"
+DEFAULT_HAIRCUT_2 = "\defaults\haircut2.jpeg"
+DEFAULT_HAIRCUT_3 = "\defaults\haircut3.jpeg"
