bd5652225889b5259a7332a56fb8543f4ce8c7c6
adaml <adamlynch95@gmail.com>
2017-03-04 18:33:12 -0700

    Fixing some sizing with portfolio again     -Not finished yet, switching to laptop

--- static/javascript/stylist_portfolio.js
+++ static/javascript/stylist_portfolio.js
@@ -2,12 +2,22 @@
  * Created by Adam on 2/23/2017.
  */
 
-$(".portfolio-image").click(function() {
-    $("#popup-image").attr("src", $(this).attr("src"));
-    $("#popup-modal").modal("show");
-
+$(function () {
+    if ($('.portfolio-image').height() > $('.portfolio-image').width()) {
+        $('.portfolio-image').height( '100%' );
+    }
+    else
+        $('.portfolio-image').width( '100%' );
+        var padding = (300 - $('.portfolio-image').height())/2;
+        console.log(padding);
+        $('.portfolio-image').css({'margin-top': padding});;
 })
 
-$(document).keyup(function(e) {
-        if ((e.keyCode ==27) && ($('#popup-modal').hasClass("show"))) $("#popup-modal").modal("hide");
-    })
+$(".portfolio-image").click(function () {
+    $("#popup-image").attr("src", $(this).attr("src"));
+    $("#popup-modal").modal("show");
+})
+
+$(document).keyup(function (e) {
+    if ((e.keyCode == 27) && ($('#popup-modal').hasClass("show"))) $("#popup-modal").modal("hide");
+})
